b711fd0c68a81c95da09887d2a2b4ebdd8154ce8
Celtico(Alan) <alanmesquita2022@gmail.com>
2023-03-31 15:11:29 -0300

    atualização dos nomes das pastas

---
+++ (Questão C)Trabalho Cálculo Númerico/src/metodos/main/Main.java
@@ -0,0 +1,44 @@
+package metodos.main;
+
+import metodos.entidades.*; // importando os metodos criados
+
+
+import constantes.Constantes; //algumas constantes
+
+public class Main extends Janela{
+
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args){
+		
+		switch(Menu.retornarEscolha()) {
+		case 1:
+			MetodoBissecao mb = new MetodoBissecao();
+			mb.bissecao();
+			new Janela(Constantes.METODO_BISSECAO, Constantes.COLUNAS_BISSECAO, mb.retornarDados()).setVisible(true);
+		break;
+		case 2:
+			MetodoFalsaPosicao fp = new MetodoFalsaPosicao();
+			fp.falsaPosicao();
+			new Janela(Constantes.METODO_FALSA_POSICAO, Constantes.COLUNAS_FALSA_POSICAO, fp.retornarDados()).setVisible(true);
+		break;
+		case 3:
+			MetodoPontoFixo pf = new MetodoPontoFixo();
+			pf.pontoFixo();
+			new Janela(Constantes.METODO_PONTO_FIXO, Constantes.COLUNAS_PONTO_FIXO, pf.retornarDados()).setVisible(true);
+		break;
+		case 4:
+			MetodoNewton mn = new MetodoNewton();
+			mn.mtdNewton();
+			new Janela(Constantes.METODO_NEWTON, Constantes.COLUNAS_NEWTON, mn.retornarDados()).setVisible(true);
+		break;
+		case 0:
+			System.out.println("Fechando programa...");
+			System.exit(0);
+		break;
+		
+	}
+
+	}
+
+}
